4bef4bd2f6e8bd4d26e7a85af678c2b8e2456637
Luvjeet19 <luvjeet.k.1@gmail.com>
2026-06-07 14:03:47 +0530

    mongodb_connencted

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), // loads .env file
+    MongooseModule.forRoot(process.env.MONGODB_URI!), // connects to MongoDB
+  ],
 })
 export class AppModule {}
